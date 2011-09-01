844130705c8b03c3e69398186538030eecf8c8ea
Marek Marczykowski <marmarek@mimuw.edu.pl>
2011-09-01 11:38:05 +0200

    initramfs: remove explicit -o ro from /lib/modules (#333)
    If /dev/xvdd is R/O, this is done automatically

--- vm-initramfs/pre-pivot/50_mount_modules.sh
+++ vm-initramfs/pre-pivot/50_mount_modules.sh
@@ -7,4 +7,4 @@
 echo "Waiting for /dev/xvdd device..."
 while ! [ -e /dev/xvdd ]; do sleep 0.1; done
 
-mount -n -t ext3 -o ro /dev/xvdd $NEWROOT/lib/modules
+mount -n -t ext3 /dev/xvdd $NEWROOT/lib/modules
